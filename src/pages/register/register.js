4c3466e0a2f8c69b094661e93d550196b0ebb391
Natali Ustinova <nata@purple.studio>
2023-01-05 13:35:35 +0200

    fix: register page input names

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -4,10 +4,10 @@ const registerHTML = Auth({
   title: 'Регистрация',
   inputs:
     [
-      { id: "email", label: "Почта", type: "email", placeholder: "Введите email", autocomplete: "on"},
+      { id: "email", label: "Почта", type: "email", placeholder: "Введите email", autocomplete: "email"},
       { id: "login", label: "Логин", type: "text", placeholder: "Введите логин", autocomplete: "on"},
-      { id: "name", label: "Имя", type: "text", placeholder: "Введите имя", autocomplete: "given-name"},
-      { id: "surname", label: "Фамилия", type: "text", placeholder: "Введите фамилию", autocomplete: "family-name"},
+      { id: "first_name", label: "Имя", type: "text", placeholder: "Введите имя", autocomplete: "given-name"},
+      { id: "second_name", label: "Фамилия", type: "text", placeholder: "Введите фамилию", autocomplete: "family-name"},
       { id: "phone", label: "Телефон", type: "tel", placeholder: "Введите телефон", autocomplete: "tel"},
       { id: "password1", label: "Пароль", type: "password", placeholder: "Введите пароль", autocomplete: "new-password" },
       { id: "password2", label: "Пароль (еще раз)", type: "password", placeholder: "Введите пароль еще раз", autocomplete: "new-password" }
