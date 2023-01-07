afcb64f3047176cf012fcf0301880cc14ce050a5
Natali Ustinova <nata@purple.studio>
2023-01-07 22:36:33 +0200

    fix: button file name

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -1,6 +1,6 @@
 import tpl from './tpl.hbs';
 import './style.scss';
-import button from '../../components/button';
+import button from '../../components/Button';
 
 
 const Auth =  ({title, inputs, buttonId, buttonValue, text, redirectId}) => {
