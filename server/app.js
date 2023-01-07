1fda37430e6b72100eda8241522578e44b086776
Natali Ustinova <nata@purple.studio>
2023-01-07 22:02:01 +0200

    feat: add sass config

--- server/app.js
+++ server/app.js
@@ -3,10 +3,14 @@ const path = require('path');
 
 const app = express();
 const PORT = 3000;
-const PATH = path.join(__dirname, '/../dist');
+const PATH = path.join(__dirname, '/../dist/index.html');
 
 app.set('view engine', 'hbs')
-app.use(express.static(PATH));
+// app.use(express.static(PATH));
+
+app.get('*', function(req, res) {
+  res.sendFile(PATH);
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
