2c95a0f9bd6e2c5372833969d30206190deeb0c6
Natali Ustinova <nata@purple.studio>
2023-01-07 22:41:54 +0200

    fix: button file name in Profile form

--- src/components/ProfileForm/index.js
+++ src/components/ProfileForm/index.js
@@ -1,7 +1,7 @@
 import Handlebars from 'handlebars';
 import tpl from './tpl.hbs';
 import './style.scss';
-import button from '../../components/button';
+import button from '../../components/Button';
 
 Handlebars.registerPartial('profileForm', tpl);
 
